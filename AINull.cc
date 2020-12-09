1852602c96632f9ff360b9bb3bc5fa496774d6a9
cuberhaus <pol@gmail.com>
2020-12-09 21:25:10 +0100

    Fixing Kelan's bug.

--- AINull.cc
+++ AINull.cc
@@ -33,7 +33,6 @@ struct PLAYER_NAME : public Player
         cola.push(p);
         while (not cola.empty())
         {
-            // p has to be the node visited
             Pos pact = cola.front();
             cola.pop();
             if (pos_ok(pact))
@@ -53,7 +52,8 @@ struct PLAYER_NAME : public Player
                     ptemp.j += mov[i][1];
                     if (pos_ok(ptemp) and cell(ptemp).type == Street and cell(ptemp).id == -1) //and not visitats[ptemp.i][ptemp.j])
                     {
-                        if (Path.find(ptemp) == Path.end()) {
+                        if (Path.find(ptemp) == Path.end())
+                        {
                             Path[ptemp] = pact;
                             cola.push(ptemp);
                         }
@@ -104,99 +104,59 @@ struct PLAYER_NAME : public Player
    */
     virtual void play()
     {
-        // if (start) {
-        //     start = false;
-        //     bazookas.resize(num_ini_bazookas());
-        //     guns.resize(num_ini_guns());
-        //     moneys.resize(num_ini_money());
-        //     foods.resize(num_ini_food());
-        //     for (int i = 0; i < num_ini_bazookas(); ++i) {
-        //         bazookas[i].i = -1;
-        //         bazookas[i].j = -1;
-        //     }
-        //     for (int i = 0; i < num_ini_guns(); ++i) {
-        //         guns[i].i = -1;
-        //         guns[i].j = -1;
-        //     }
-        //     for (int i = 0; i < num_ini_money(); ++i) {
-        //         moneys[i].i = -1;
-        //         moneys[i].j = -1;
-        //     }
-        //     for (int i = 0; i < num_ini_food(); ++i) {
-        //         foods[i].i = -1;
-        //         foods[i].j = -1;
-        //     }
-        // }
-
-        // n = board max column, m = board max row
-        // int n = board_cols();
-        // int m = board_rows();
-        // for (int i = 0; i < n; ++i)
-        // {
-        //     for (int j = 0; j < m; ++j)
-        //     {
-        //         Cell casilla = cell(i, j);
-        //         if (casilla.weapon == Bazooka) {
-        //
-        //         }
-        //         cerr << casilla.type << ' '; // cout << street or building
-        //     }
-        //     cerr << endl;
-        // }
-
         // INICIALITZAR INVARIANTS
         Path.clear();
-        //if (is_day())
-        //{
-        vector<int> w = warriors(me());
-        bool bexist = true;
-        bool gexist = true;
-        bool mexist = true;
-        bool bdone = false;
-        bool gdone = false;
-        bool mdone = false;
-        for (int id : w)
-        { // iterate over warriors
-            Pos pciti = citizen(id).pos;
-            bdone = false;
-            gdone = false;
-            mdone = false;
-            if (bexist)
-            {
-                Pos pobj = BFS(pciti, 'b'); // buscamos bazokas
-                bdone = true;
-                if (pobj != Pos(-1, -1))
+        if (is_day())
+        {
+            vector<int> w = warriors(me());
+            bool bexist = true;
+            bool gexist = true;
+            bool mexist = true;
+            bool bdone = false;
+            bool gdone = false;
+            bool mdone = false;
+            for (int id : w)
+            { // iterate over warriors
+                Pos pciti = citizen(id).pos;
+                bdone = false;
+                gdone = false;
+                mdone = false;
+                if (bexist)
                 {
-                    Dir d = direccionBFS(pciti, pobj);
-                    if (pos_ok(pciti + d))
+                    Pos pobj = BFS(pciti, 'b'); // buscamos bazokas
+                    bdone = true;
+                    if (pobj != Pos(-1, -1))
                     {
-                        move(id, d);
+                        Dir d = direccionBFS(pciti, pobj);
+                        if (pos_ok(pciti + d))
+                        {
+                            move(id, d);
+                        }
+                    }
+                    else
+                    {
+                        bexist = false;
                     }
                 }
-                else
+                else if (gexist)
                 {
-                    bexist = false;
-                }
-            }
-            else if (gexist)
-            {
-                Pos pobj = BFS(pciti, 'g');
-                gdone = true;
-                if (pobj != Pos(-1, -1))
-                {
-                    Dir d = direccionBFS(pciti, pobj);
-                    if (pos_ok(pciti + d))
+                    Pos pobj = BFS(pciti, 'g');
+                    gdone = true;
+                    if (pobj != Pos(-1, -1))
                     {
-                        move(id, d);
+                        Dir d = direccionBFS(pciti, pobj);
+                        if (pos_ok(pciti + d))
+                        {
+                            move(id, d);
+                        }
+                    }
+                    else
+                    {
+                        gexist = false;
                     }
                 }
-                else
-                {
-                    gexist = false;
-                }
+                Path.clear();
             }
-            Path.clear();
-        }
             //}
 
             //     // At day take care of builders
@@ -242,52 +202,24 @@ struct PLAYER_NAME : public Player
                         gexist = false;
                     }
                 }
-            Path.clear();
+                Path.clear();
             }
-            // }
-            // else
-            // { // night time
-            //     vector<int> w = warriors(me());
-            //     for (int id : w)
-            //     { // iterate over warriors
-            //         Pos pciti = citizen(id).pos;
-            //         //cerr << "citizen " << id << " in position: " << pciti << endl;
-            //         char c = 'b'; // busquem bazokas
-            //         Pos pobj = BFS(pciti, c);
-            //         if (pobj != Pos(-1, -1))
-            //         {
-            //             Dir d = direccionBFS(pciti, pobj);
-            //             //Dir d = Down; // prova
-            //             if (pos_ok(pciti + d))
-            //             {
-            //                 move(id, d);
-            //             }
-            //         }
-            //     }
+        }
+        else
+        { // night time
+            vector<int> w = warriors(me());
+            for (int id : w)
+            { // iterate over warriors
+            }
 
-            //     // At day take care of builders
-            //     vector<int> b = builders(me());
-            //     for (int id : b)
-            //     { // iterate over builders
-            //         Pos pciti = citizen(id).pos;
-            //         //cerr << "citizen " << id << " in position: " << pciti << endl;
-            //         char c = 'b'; // busquem bazokas
-            //         Pos pobj = BFS(pciti, c);
-            //         if (pobj != Pos(-1, -1))
-            //         {
-            //             Dir d = direccionBFS(pciti, pobj);
-            //             //Dir d = Down; // prova
-            //             if (pos_ok(pciti + d))
-            //             {
-            //                 move(id, d);
-            //             }
-            //         }
-            //     }
-        
-    }
-};
+            vector<int> b = builders(me());
+            for (int id : b)
+            {
+            }
+        }
+    };
 
-/**
+    /**
  * Do not modify the following line.
  */
-RegisterPlayer(PLAYER_NAME);
+    RegisterPlayer(PLAYER_NAME);
